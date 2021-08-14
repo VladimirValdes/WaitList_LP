c592e8f1adfd7c4a65d5b72f6d4c6b1a8edbd3cf
Vladimir Valdes <mendozahum20@gmail.com>
2021-08-14 13:49:17 -0600

    responsive menu

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+const iconMenu = document.querySelector('#icon-menu');
+const menu = document.querySelector('#menu');
+
+iconMenu.addEventListener('click', () => menu.classList.toggle('menu--active'));
